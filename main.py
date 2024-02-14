b31937581d0db303829284704b3dd373e3d0d604
Poramet Keawubol <6610110554@email.psu.ac.th>
2024-02-14 22:29:45 +0700

    create background and upload images and sounds

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from  kivy.uix.widget import Widget
+
+
+# เราควรทำเกมขาวดำหรือแบบมีสีสันดีอ่ะB1
+class Background(Widget):
+    pass
+
+
+class MainApp(App):
+    pass
+
+MainApp().run()

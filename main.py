f54d533d94f0175a37a857debc128be6d6c45265
ปรเมษ แก้วอุบล <keawubolporamet505@gmail.com>
2024-03-12 21:43:46 +0700

    cloud scroll

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from kivy.clock import Clock
 from kivy.graphics import Ellipse, Rectangle
 from kivy.uix.label import Label
 from random import randint
-from kivy.graphics import Rectangle
 
 class Background(Widget):
     cloud_texture = ObjectProperty(None)
@@ -36,7 +35,12 @@ class Background(Widget):
 
     def scroll_textures(self, time_passed):
         # Set up cloud movement
-        pass  # Add your cloud movement logic here
+        self.cloud_1.pos = (self.cloud_1.pos[0] + time_passed * 50, self.cloud_1.pos[1])
+        self.cloud_2.pos = (self.cloud_2.pos[0] + time_passed * 50, self.cloud_2.pos[1])
+        if self.cloud_1.pos[0] > Window.width:
+            self.cloud_1.pos = (-200, Window.height * 0.75)
+        if self.cloud_2.pos[0] > Window.width:
+            self.cloud_2.pos = (-150, Window.height * 0.6)
 
 class Dinosaur(Image):
     is_jumping = False
@@ -72,7 +76,6 @@ class Dinosaur(Image):
         if not self.is_jumping:
             self.is_jumping = True
             self.velocity_y = self.jump_speed
-from kivy.graphics import Rectangle
 
 class Obstacle(Widget):
     def __init__(self, **kwargs):
@@ -85,7 +88,7 @@ class Obstacle(Widget):
         # Set up cactus image
         with self.canvas:
             self.floor_texture = Rectangle(source='images/floor.png', size=self.size, pos=self.pos)
-            self.cactus_texture = Rectangle(source='images/cactus-big.png', size=self.size, pos=(self.x, self.y ))
+            self.cactus_texture = Rectangle(source='images/cactus-big.png', size=self.size, pos=(self.x, self.y))
 
     def update(self, dt):
         self.x -= self.velocity_x * dt
@@ -100,14 +103,12 @@ class Obstacle(Widget):
         self.cactus_texture.pos = (self.x, self.y + 100)  # Reset cactus position along with the obstacle
         self.floor_texture.pos = self.pos  # Reset floor position along with the obstacle
 
-
-
 class Point(Widget):
     score = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        Clock.schedule_interval(self.update_score, 0.07)  # Update score 
+        Clock.schedule_interval(self.update_score, 0.07)  # Update score
 
     def update_score(self, dt):
         self.parent.background.score_label.text = str(self.score)  # Update displayed score
@@ -125,6 +126,7 @@ class Game(Widget):
         self.add_widget(self.obstacle)
         self.add_widget(self.point)  # Add Point widget to the game
         Clock.schedule_interval(self.update, 1 / 60)
+        Clock.schedule_interval(self.background.scroll_textures, 0.1)  # Scroll textures every 0.1 seconds
 
     def update(self, dt):
         self.dinosaur.update(dt)
@@ -136,7 +138,7 @@ class Game(Widget):
         self.remove_widget(self.dinosaur)
         self.remove_widget(self.obstacle)
         self.add_widget(self.game_over_label)
-        
+
     def on_touch_down(self, touch):
         self.dinosaur.jump()
 
